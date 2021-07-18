2e6086361144586136ba436ea24f09d897c7e5cb
Katie <katie.wy.wu@gmail.com>
2021-07-17 23:18:42 -0700

    Create EnableGameControls.cs
    ** In case we may want specific game controls to be enabled when mouse is hovered over that specific section of the game.

---
+++ Assets/Scripts/EnableGameControls.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableGameControls : MonoBehaviour
+{
+    void OnMouseOver()
+    {
+        Debug.Log(this.gameObject.name);
+    }
+}
