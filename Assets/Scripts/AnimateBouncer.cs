13eb2e87198684776bd315211572de1082616d4c
Katie <katie.wy.wu@gmail.com>
2021-07-19 21:15:05 -0700

    Rename AnimateBouncer.cs class from "TriggerBouncer" to "AnimateBouncer", and disable "IsTrigger" on Collider.

--- Assets/Scripts/AnimateBouncer.cs
+++ Assets/Scripts/AnimateBouncer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggerBouncer : MonoBehaviour
+public class AnimateBouncer : MonoBehaviour
 {
     public Animator animator;
 
