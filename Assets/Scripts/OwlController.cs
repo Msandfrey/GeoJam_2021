694f7b7f984ed6ff6bf828aed7ca992b382de010
sawada14 <matthewsandfrey@gmail.com>
2021-07-21 11:15:02 -0700

    So you can test the owl

--- Assets/Scripts/OwlController.cs
+++ Assets/Scripts/OwlController.cs
@@ -58,7 +58,7 @@ public class OwlController : MonoBehaviour
                 rb.velocity = -1 * speed * Vector3.up;
             }
             moving = true;
-            owlAnimator.StartFlying();
+            //owlAnimator.StartFlying();
             //down = !down;
             //rb.velocity = speed * Vector3.up * (down ? -1 : 1);
         }
@@ -89,7 +89,7 @@ public class OwlController : MonoBehaviour
         //down = !down;
         //rb.velocity = speed * Vector3.up * (down ? -1 : 1);
         moving = true;
-        owlAnimator.StartFlying();
+        //owlAnimator.StartFlying();
         audioManager.PlayAudioClip(moveAudioClip);
     }
     private void OnTriggerEnter(Collider other)
@@ -97,7 +97,7 @@ public class OwlController : MonoBehaviour
         Debug.Log("i fucking made it hoot");
         rb.velocity = Vector3.zero;
         moving = false;
-        owlAnimator.StopFlying();
+        //owlAnimator.StopFlying();
         if (down)
         {
             isAtBottom = true;
