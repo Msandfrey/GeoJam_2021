138bd9975840be2c75011dce0645c23af3fb815f
Katie <katie.wy.wu@gmail.com>
2021-07-20 21:11:45 -0700

    Implement a skewed angle of ball's bounce when it hits the bar.
    - Delete ClickandHold.cs
    - Create OnCollisionEnter() to account for angle offset of ball when hitting the far left/right of bar.

--- Assets/Scripts/BreakoutBar.cs
+++ Assets/Scripts/BreakoutBar.cs
@@ -5,16 +5,47 @@ using UnityEngine;
 public class BreakoutBar : MonoBehaviour
 {
     Vector2 mousePos;
-    // Start is called before the first frame update
+    [SerializeField] public float initialBallSpeed;
+    [SerializeField] public float ballAngleOffset;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector3(mousePos.x, transform.position.y, 0);
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name.Contains("Ball"))
+        {
+            Rigidbody rbBall = col.gameObject.GetComponent<Rigidbody>();
+            // get ball hit point
+            Vector3 hitPoint = col.contacts[0].point;
+            
+            // get center of bar
+            Vector3 barCenter = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y); 
+
+            rbBall.velocity = Vector3.zero;
+
+            // measures how far the ball's hit is from the center of the bar
+            float diff = barCenter.x - hitPoint.x; 
+
+            // hitting the left side of the bar
+            if (hitPoint.x < barCenter.x) 
+            {
+                rbBall.AddForce(new Vector2(-(Mathf.Abs(diff * ballAngleOffset)), initialBallSpeed));
+
+            }
+            // not hitting the left side (so either right, or near center)
+            else 
+            {
+                rbBall.AddForce(new Vector2((Mathf.Abs(diff * ballAngleOffset)), initialBallSpeed));
+            }
+        }
     }
 }
